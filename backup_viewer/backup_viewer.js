f122cd55d48d7106482922c67f5fe8fbbf76319a
Ewan Howell <48070995+ewanhowell5195@users.noreply.github.com>
2025-02-28 21:21:18 +0000

    backup viewer v2.0

--- backup_viewer/backup_viewer.js
+++ backup_viewer/backup_viewer.js
@@ -1,185 +1,182 @@
 (() => {
-  let styles, backupList
-  const backupPath = app.getPath("userData") + osfs + "backups"
-  const E = s => $(document.createElement(s))
+  let styles, oldTemplate
   Plugin.register("backup_viewer", {
     title: "Backup Viewer",
-    icon: "fa-archive",
+    icon: "icon.png",
     author: "Ewan Howell",
-    description: "View the project backups from within Blockbench.",
-    about: "Add a backup tab to the Blockbench start screen that allows you to view a list of the project backups from within Blockbench.",
-    tags: ["Backups", "Blockbench"],
-    version: "1.0.0",
-    min_version: "4.5.2",
+    description: "View the project backups from the Blockbench start screen.",
+    tags: ["Backups", "Start screen", "Blockbench"],
+    version: "2.0.0",
+    min_version: "4.12.0",
     variant: "desktop",
+    creation_date: "2022-12-28",
+    has_changelog: true,
+    website: "https://ewanhowell.com/plugins/backup-viewer/",
+    repository: "https://github.com/ewanhowell5195/blockbenchPlugins/tree/main/backup-viewer",
+    bug_tracker: "https://github.com/ewanhowell5195/blockbenchPlugins/issues/new?title=[Backup Viewer]",
     onload() {
+      const backupPath = app.getPath("userData") + osfs + "backups"
+
+      const backups = []
+      function getBackups() {
+        console.log("loading backups")
+        backups.length = 0
+        const backupFilePaths = fs.readdirSync(backupPath)
+        for (const file of backupFilePaths) {
+          const path = PathModule.join(backupPath, file)
+          const stats = fs.statSync(path)
+          backups.push({
+            id: file,
+            path,
+            name: file.replace(/backup_\d+\.\d+\.\d+_\d+\.\d+_?/, "").replace(/\.bbmodel$/, "") || 'no name',
+            date: stats.mtime.toLocaleDateString(),
+            time: stats.mtime.toLocaleTimeString().replace(/:\d+ /, " "),
+            size: `${separateThousands(Math.round(stats.size / 1024))} KB`,
+            timestamp: stats.mtime.getTime()
+          })
+        }
+        backups.sort((a, b) => b.timestamp - a.timestamp)
+      }
+      getBackups()
+
       styles = Blockbench.addCSS(`
-        #backup-tabs {
-          background-color: var(--color-ui);
-          position: absolute;
-          top: -6px;
+        #start-screen-recent-tabs {
           display: flex;
-          gap: 2px;
+          gap: 10px;
+          padding-bottom: 5px;
+          margin-top: -5px;
+
+          h2 {
+            cursor: pointer;
+            padding: 0 10px;
+            margin-top: -3px;
+
+            &:hover {
+              color: var(--color-light);
+            }
+          }
+
+          .selected {
+            border-bottom: 3px solid var(--color-accent);
+          }
         }
-        .backup-tab {
-          border-bottom: 3px solid transparent;
-          cursor: pointer;
-          padding: 4px 20px 0;
-          font-size: 1.75rem;
+
+        .recent-project-list .backup .icon_wrapper {
+          padding-left: 22px;
+          height: 22px;
+          margin: 0 !important;
         }
-        .backup-tab-active {
-          border-bottom: 3px solid var(--color-accent);
-          cursor: initial;
-        }
-        .backup-tab:hover {
-          background-color: var(--color-selected);
-        }
-        .backup-tab-active:hover {
-          background-color: var(--color-ui);
-        }
-        .backup-backup-list {
-          margin-left: 0!important;
+
+        .recent-project-list .backup .recent_project_date {
           display: grid;
+          grid-template-columns: 76px 56px 60px;
+          gap: 16px;
+
+          :last-child {
+            text-align: right;
+          }
         }
-        .backup-list-inactive {
-          display: none!important;
+
+        .recent_list_grid .backup {
+          padding: 10px 20px 12px 20px;
+          height: initial !important;
+
+          &:hover * {
+            color: var(--color-accent_text);
+          }
         }
-        .backup-item {
-          background-color: var(--color-back);
-          padding: 10px 20px;
-          overflow: hidden;
-        }
-        .backup-item, .backup-item * {
-          cursor: pointer;
-        }
-        .backup-item:hover {
-          background-color: var(--color-accent);
-          color: var(--color-accent_text);
-        }
-        .backup-item > i {
-          display: none;
-        }
-        .backup-item > div {
-          font-size: 16px;
-          color: var(--color-subtle_text);
-          position: relative;
-        }
-        .backup-item:hover > div {
-          color: var(--color-bright_ui_text);
-        }
-        .backup-item-hidden {
-          display: none!important;
-        }
-        .backup-list-display-list {
-          display: block;
-        }
-        .backup-list-display-list > .backup-item {
-          background-color: initial;
-          padding: 0 0 0 12px;
+
+        .recent_list_grid .backup .recent_project_date {
+          font-size: 14px;
           display: flex;
-          height: 26.39px;
-          align-items: center;
-          margin: 2px 0;
-        }
-        .backup-list-display-list > .backup-item:hover {
-          color: var(--color-light);
-        }
-        .backup-list-display-list i {
-          display: initial;
-        }
-        .backup-list-display-list h3 {
-          font-size: 17.6px;
-          margin: 4px 0 0 4px!important;
-          padding: 0!important;
-        }
-        .backup-list-display-list span {
-          flex: 1;
-        }
-        .backup-list-display-list div::before {
-          content: "";
-          position: absolute;
-          width: 16px;
-          height: 26px;
-          margin-left: -20px;
-          background: linear-gradient(90deg, transparent, var(--color-ui));
+          gap: 8px;
+
+          &::before {
+            display: none;
+          }
+
+          :last-child {
+            flex: 1;
+            text-align: right;
+          }
         }
       `)
-      $(".start_screen_right > h2").css("position", "relative").append(
-        E("div").attr("id", "backup-tabs").append(
-          E("div").text("Recent").attr("id", "backup-recent-tab").addClass("backup-tab backup-tab-active"),
-          E("div").text("Backups").attr("id", "backup-backup-tab").addClass("backup-tab")
-        )
+
+      StartScreen.vue.$destroy()
+
+      StartScreen.vue.$data.recentsView = "backups"
+      StartScreen.vue.$data.backups = backups
+
+      StartScreen.vue.$options.methods.openBackup = backup => Blockbench.read([backup.path], {}, files => loadModelFile(files[0]))
+      StartScreen.vue.$options.methods.getBackups = getBackups
+
+      oldTemplate = StartScreen.vue.$options.template
+
+      const newTemplate = oldTemplate.replace(
+        `<h2>${tl('mode.start.recent')}</h2>`,
+        `
+          <div id="start-screen-recent-tabs">
+            <h2 :class="{ selected: recentsView === 'recents' }" @click="recentsView = 'recents'">${tl("mode.start.recent")}</h2>
+            <h2 :class="{ selected: recentsView === 'backups' }" @click="getBackups(); recentsView = 'backups'">Backups</h2>
+          </div>
+        `
+      ).replace(
+        `<div v-if="redact_names">{{ '['+tl('generic.redacted')+']' }}</div>`,
+        `
+          <div v-if="redact_names">{{ '['+tl('generic.redacted')+']' }}</div>
+          <ul v-else-if="recentsView === 'backups' && list_type === 'list'" class="recent-project-list">
+            <li v-for="backup in backups" class="recent_project backup" @click="openBackup(backup)">
+              <span class="icon_wrapper" v-html="getIconNode('fa-archive').outerHTML"></span>
+              <span class="recent_project_name">{{ backup.name }}</span>
+              <span class="recent_project_date">
+                <span>{{ backup.date }}</span>
+                <span>{{ backup.time }}</span>
+                <span>{{ backup.size }}</span>
+              </span>
+            </li>
+          </ul>
+          <ul v-else-if="recentsView === 'backups'" class="recent_list_grid">
+            <li v-for="backup in backups" class="recent_project thumbnail backup" @click="openBackup(backup)">
+              <h3 class="backup-project-name">{{ backup.name }}</h3>
+              <span class="recent_project_date">
+                <span>{{ backup.date }}</span>
+                <span>{{ backup.time }}</span>
+                <span>{{ backup.size }}</span>
+              </span>
+            </li>
+          </ul>
+        `
       )
-      backupList = E("ul").addClass("backup-backup-list backup-list-inactive").insertAfter($(".start_screen_right > ul"))
-      if ($("#start_screen_view_menu > .tool").eq(1).hasClass("selected")) backupList.addClass("backup-list-display-list")
-      $(".backup-tab").on("click", e => {
-        $(".backup-tab-active").removeClass("backup-tab-active")
-        $(".start_screen_right > ul").addClass("backup-list-inactive")
-        const tab = $(e.currentTarget).addClass("backup-tab-active")
-        const selected = tab.attr("id").split("-")[1]
-        if (tab.attr("id") === "backup-recent-tab") $(".start_screen_right > ul").first().removeClass("backup-list-inactive")
-        else backupList.removeClass("backup-list-inactive")
+
+      StartScreen.vue = new Vue({
+        el: "#start_screen",
+        components: StartScreen.vue.$options.components,
+        data: { ...StartScreen.vue.$data },
+        methods: StartScreen.vue.$options.methods,
+        computed: StartScreen.vue.$options.computed,
+        mounted: StartScreen.vue.$options.mounted,
+        template: newTemplate
       })
-      const files = fs.readdirSync(backupPath)
-      const backups = []
-      for (const file of files) if (file.endsWith(".bbmodel")) {
-        const m = file.match(/^backup_(?<day>\d{1,2})\.(?<month>\d{1,2})\.(?<year>\d\d)_(?<hour>\d{1,2})\.(?<minute>\d{1,2})(?:_(?<name>.+))?\.bbmodel$/)?.groups
-        if (!m) continue
-        const date = new Date(Date.parse(`20${m.year}/${m.month}/${m.day} ${m.hour}:${m.minute}`))
-        backups.push({
-          name: m.name ?? "untitled",
-          path: backupPath + osfs + file,
-          date
-        })
-      }
-      backups.sort((a, b) => b.date - a.date)
-      $("#start_screen_view_menu > .search_bar").on("click", processSearch)
-      const searchBar = $("#start_screen_view_menu > .search_bar > input").on("input", processSearch)
-      const searchText = searchBar.val()
-      for (const backup of backups) {
-        const item = E("li").addClass("backup-item").attr({
-          title: backup.path,
-          "data-name": backup.name.toLowerCase()
-        }).append(
-          E("i").addClass("fa_big icon fa fa-archive"),
-          E("h3").text(backup.name),
-          E("span"),
-          E("div").text(`${backup.date.getDate().toString().padStart(2, 0)}/${(backup.date.getMonth() + 1).toString().padStart(2, 0)}/${(backup.date.getYear() - 100).toString().padStart(2, 0)} ${backup.date.getHours().toString().padStart(2, 0)}:${backup.date.getMinutes().toString().padStart(2, 0)}`)
-        ).on("click", e => Blockbench.read([backup.path], {}, files => loadModelFile(files[0]))).appendTo(backupList)
-        if (!backup.name.includes(searchText)) item.addClass("backup-item-hidden")
-      }
-      $("#start_screen_view_menu > .tool").first().on("click", gridView)
-      $("#start_screen_view_menu > .tool").eq(1).on("click", listView)
-      processSearch()
     },
     onunload() {
-      $("#start_screen_view_menu > .search_bar").off("click", processSearch)
-      $("#start_screen_view_menu > .search_bar > input").off("input", processSearch)
-      $("#start_screen_view_menu > .tool").first().off("click", gridView)
-      $("#start_screen_view_menu > .tool").eq(1).off("click", listView)
+      StartScreen.vue.$destroy()
+
+      delete StartScreen.vue.$data.recentsView
+      delete StartScreen.vue.$data.backups
+      delete StartScreen.vue.$options.methods.openBackup
+      delete StartScreen.vue.$options.methods.getBackups
+
+      StartScreen.vue = new Vue({
+        el: "#start_screen",
+        components: StartScreen.vue.$options.components,
+        data: { ...StartScreen.vue.$data },
+        methods: StartScreen.vue.$options.methods,
+        computed: StartScreen.vue.$options.computed,
+        mounted: StartScreen.vue.$options.mounted,
+        template: oldTemplate
+      })
+
       styles.delete()
-      $("#backup-tabs").remove()
-      $(".backup-backup-list").remove()
-      $(".backup-list-inactive").removeClass("backup-list-inactive")
     }
   })
-  function processSearch() {
-    const text = $("#start_screen_view_menu > .search_bar > input").val().toLowerCase()
-    $(".backup-item").each(function() {
-      const item = $(this)
-      if (!item.attr("data-name").includes(text)) item.addClass("backup-item-hidden")
-      else item.removeClass("backup-item-hidden")
-    })
-  }
-  function gridView() {
-    backupList.removeClass("backup-list-display-list")
-    hideRecent()
-  }
-  function listView() {
-    backupList.addClass("backup-list-display-list")
-    hideRecent()
-  }
-  function hideRecent() {
-    if ($("#backup-backup-tab").hasClass("backup-tab-active")) $(".start_screen_right > ul").first().addClass("backup-list-inactive")
-  }
 })()
